38bda0d69e4d41a9b1757b44dee43e449c7be78d
cklam12345 <cklam12345@gmail.com>
2024-09-04 01:01:11 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# mem0poc, mem 0 is super simple way to include memory for your ai assistant you are building, try app.py
---
+++ ci.sh
@@ -0,0 +1,7 @@
+echo "# mem0poc, mem 0 is super simple way to include memory for your ai assistant you are building, try app.py" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:cklam12345/mem0poc.git
+git push -u origin main
